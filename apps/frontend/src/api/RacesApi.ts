d8762f75b02eca21cdab54b4f317dd06d79d4538
Adam Sporczyk <adamspo00@gmail.com>
2026-03-06 20:20:13 +0100

    Remove API_URL fallback

--- apps/frontend/src/api/RacesApi.ts
+++ apps/frontend/src/api/RacesApi.ts
@@ -1,6 +1,6 @@
 import { type Race } from 'types/Race'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
+const API_URL = import.meta.env.VITE_API_URL
 
 interface RacesResponse {
   races: Race[]
